887d1152010c2333194e2a434f98b8f6d6cbe2fe
codingwithvarsha <varsharotti5@gmail.com>
2024-04-16 15:01:49 +0530

    modified folders

--- mscript.js
+++
@@ -1,5 +0,0 @@
-function aunthentication(event){
-    event.preventDefault();
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
-}
