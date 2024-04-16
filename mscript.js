35bb2952a7fcc5014eccb489b0daeed83c2dbee5
codingwithvarsha <varsharotti5@gmail.com>
2024-04-16 13:48:12 +0530

    project started

---
+++ mscript.js
@@ -0,0 +1,5 @@
+function aunthentication(event){
+    event.preventDefault();
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+}
