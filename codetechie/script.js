24ea7f4a95b46a814d252b4d5fcee8064ea91e5c
codetechie43 <sujalkhade676@gmail.com>
2024-04-18 20:22:51 +0530

    changes to universal

---
+++ codetechie/script.js
@@ -0,0 +1,7 @@
+// Function to toggle between light and dark themes
+// function toggleTheme() {
+    // const body = document.body;
+    // body.classList.toggle('dark-theme');
+// }
+
+// toggleTheme();
