2140be5d419b20c397d2251ee4940e93059926b8
codingwithvarsha <varsharotti5@gmail.com>
2024-05-23 19:15:16 +0530

    added a toggle theme button

---
+++ codewithvarsha/script.js
@@ -0,0 +1,8 @@
+ //Function to toggle between light theme and dark theme
+// function toggleTheme() {
+//     const body = document.body;
+//     body.classList.toggle('dark theme');
+
+// }
+
+// toggleTheme();
